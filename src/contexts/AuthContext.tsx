7b4a8449acff360685b5c53a83a4d7d9fd759c1c
Igor Cerqueira <igor.cerqueira@jussi.com.br>
2022-11-03 14:09:23 -0300

    expo auth session, expo web browser initial config

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,4 +1,9 @@
-import { createContext, ReactNode } from 'react';
+import { createContext, ReactNode, useState } from 'react';
+import * as AuthSession from 'expo-auth-session';
+import * as Google from 'expo-auth-session/providers/google';
+import * as WebBrowser from 'expo-web-browser';
+
+WebBrowser.maybeCompleteAuthSession();
 
 interface User {
   name: string;
@@ -11,20 +16,39 @@ interface AuthProviderProps {
 
 export interface AuthContextDataProps {
   user: User;
+  isUserLoading: boolean;
   signIn: () => Promise<void>;
 }
 
 export const AuthContext = createContext({} as AuthContextDataProps);
 
 export function AuthContextProvider({ children }: AuthProviderProps) {
+  const [isUserLoading, setIsUserLoading] = useState(false);
+
+  const authKey = AuthSession.makeRedirectUri({ useProxy: true });
+  const [request, response, promptAsync] = Google.useAuthRequest({
+    clientId:
+      '244619104415-68lh3ifuv1oik5qqivrpeemd0f312goj.apps.googleusercontent.com',
+    redirectUri: authKey,
+    scopes: ['profile', 'email'],
+  });
+
   async function signIn() {
-    console.log('logou');
+    try {
+      setIsUserLoading(true);
+      await promptAsync();
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsUserLoading(false);
+    }
   }
 
   return (
     <AuthContext.Provider
       value={{
         signIn,
+        isUserLoading,
         user: {
           name: 'igor',
           avatar: 'https://github.com/vieiraigor8787.png',
