f616f8bf206e1d72c631a786e153061fbc4a034d
Igor Cerqueira <igor.cerqueira@jussi.com.br>
2022-11-03 15:43:05 -0300

    text adjustment

--- src/screens/Bets.tsx
+++ src/screens/Bets.tsx
@@ -1,4 +1,4 @@
-import { Icon, VStack } from 'native-base';
+import { Icon, Text, VStack } from 'native-base';
 import { Octicons } from '@expo/vector-icons';
 import { Button } from '../componets/Button';
 import { Header } from '../componets/Header';
@@ -20,6 +20,11 @@ export function Bets() {
           leftIcon={<Icon as={Octicons} name="search" color="black" />}
         />
       </VStack>
+      <Text color="white" textAlign="center" px={10}>
+        Você ainda não está participando de {'\n'} nenhum bolão, que tal{' '}
+        <Text color="yellow.500">buscar um por código</Text> {'\n'} ou{' '}
+        <Text color="yellow.500">criar um novo</Text>?
+      </Text>
     </VStack>
   );
 }
